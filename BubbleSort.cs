76f540e660030f41e22318bb7a0a4cbddfe45548
Arun Kumar <76198217+ArunCodeGit@users.noreply.github.com>
2023-06-11 22:50:55 +0530

    Add files via upload

---
+++ BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    internal class BubbleSort
+    {
+        #region Bubble sort to sort the array ascending order.
+        public static int[] Bubble_Sort(int[] MyArray)
+        {
+            for(int i=0;i<MyArray.Length-1;i++)
+            {
+                for(int j=0; j<MyArray.Length-1; j++)
+                {
+                    if (MyArray[j] > MyArray[j+1])
+                    {
+                        int temp = MyArray[j+1];
+                        MyArray[j+1] = MyArray[j];
+                        MyArray[j] = temp;
+                    }
+                }
+            }
+            return MyArray;
+        }
+        #endregion
+    }
+}
