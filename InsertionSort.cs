e93f5fdd2200deb7f023ccd8d20e2c5b5c9a4b87
Arun Kumar <76198217+ArunCodeGit@users.noreply.github.com>
2023-06-13 22:50:26 +0530

    Add files via upload

---
+++ InsertionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    internal class InsertionSort
+    {
+        #region Insertion Sort
+        public static void Insertion_Sort(int[] MyArray)
+        {
+            for (int i = 1; i <= MyArray.Length - 1; i++)
+            {
+                int j;
+                int temp = MyArray[i];
+                for (j = i - 1; j >= 0 && MyArray[j] > temp; j--)
+                {
+                    MyArray[j + 1] = MyArray[j];
+                }
+                MyArray[j + 1] = temp;
+            }
+            foreach (int item in MyArray)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.ReadLine();
+        }
+        #endregion
+    }
+}
