a0c9149a97bf7b5fa8d3a21bb882704900bfb934
Arun Kumar <76198217+ArunCodeGit@users.noreply.github.com>
2023-06-14 23:13:24 +0530

    Add files via upload

---
+++ SelectionSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    internal class SelectionSort
+    {
+        #region Selection sort to sort the array ascending order.
+        public static void Selection_Sort(int[] MyArray)
+        {
+            for(int i=0;i<MyArray.Length;i++)
+            {
+                int minIndex = i;
+                for(int j=i+1;j<MyArray.Length;j++)
+                {
+                    if (MyArray[j] < MyArray[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                if(i!=minIndex)
+                {
+                    int temp = MyArray[i];
+                    MyArray[i] = MyArray[minIndex];
+                    MyArray[minIndex] = temp;
+                }
+                foreach (int item in MyArray)
+                {
+                    Console.Write(item + ", ");
+                }
+                Console.WriteLine();
+            }            
+            //return MyArray;
+        }
+        #endregion
+    }
+}
